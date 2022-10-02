05d3b2e7fa2fcb810e688d8baf4a7649f4f8f3c4
Kali <sezerburkas@gmail.com>
2022-10-02 09:30:58 -0400

    add: new design and contenty

--- dist/js/theme.js
+++ dist/js/theme.js
@@ -3,6 +3,8 @@ gsap.registerPlugin(ScrollTrigger);
 const tl_header = gsap.timeline();
 const tl_right = gsap.timeline();
 const tl_left = gsap.timeline();
+const tl_down = gsap.timeline();
+const tl_fade = gsap.timeline();
 
 tl_header.to(".header h1",{
     duration: 1.5,
@@ -38,9 +40,21 @@ tl_left.to(".second .right", {
     opacity: 1,
 }).to(".second .card-content",{
     y: 0,
-    duration: .5,
+    duration: 1.5,
     opacity: 1,
 }, .4);
+
+tl_down.to(".third .horizontal-cards",{
+    y: 0,
+    duration:1,
+    opacity:1
+});
+
+tl_fade.to(".yoyo .single-card",{
+    opacity:1,
+    width:"70%",
+    duration:1.6
+});
 
 ScrollTrigger.create({
     animation: tl_right,
@@ -51,6 +65,18 @@ ScrollTrigger.create({
     animation: tl_left,
     trigger: ".second"
 });
+
+ScrollTrigger.create({
+    animation: tl_down,
+    trigger: ".third"
+});
+
+
+ScrollTrigger.create({
+    animation: tl_fade,
+    trigger: ".yoyo"
+});
+
 
 
 
